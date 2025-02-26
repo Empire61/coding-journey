aa3c7aa5f558db5cd92a03632f6df9596f345144
Empire61 <evansezesinachi@gmail.com>
2025-02-26 06:36:26 -0800

    splashScreenNotes

--- lecstu/lib/splash_screen.dart
+++ lecstu/lib/splash_screen.dart
@@ -9,36 +9,49 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _fadedanimation;
+  late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
     super.initState();
 
+    //Animation Controller (controls faded effect)
     _controller = AnimationController(
+      duration: Duration(seconds:3), // Fade-in duration
       vsync: this,
-      duration: Duration(seconds: 3),
     );
 
-    _fadedanimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    // Fade effect from 0 (invincible) to 1 (fully visible)
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
 
+    //Start the animation
     _controller.forward();
 
-    Timer(Duration(seconds: 4), () {
+    //Navigate to home screen after 4 seconds
+    Timer(Duration(seconds: 4),() {
       Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) =>HomeScreen()),
+        context, 
+        MaterialPageRoute(builder: (context) => HomeScreen()),
       );
     });
+
   }
 
   @override
-  Widget build(BuildContext context) {
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
-      body: FadeTransition(
-        opacity: _fadedanimation,
+        body: FadeTransition(
+        opacity: _fadeAnimation,
         child: Container(
           width: double.infinity,
           height: double.infinity,
@@ -46,7 +59,7 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
             image: DecorationImage(
               image: AssetImage("assets/img/comb.png"),
               fit: BoxFit.contain,
-            ),
+              ),
           ),
         ),
       ),
